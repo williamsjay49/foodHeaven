f89b9d080a0531c52450a7187adacb62e3950fc7
johnson williams <williams.gb55@gmail.com>
2025-05-21 20:07:46 +0900

    header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <Hero />
+      {/* <Hero /> */}
     </>
   );
 }
--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -1,22 +1,39 @@
 import { navLinks } from "../constants/index.js";
+import React, { useEffect } from "react";
 import Button from "../components/Button.jsx";
+
 const NavBar = () => {
+  const [scrolled, setScrolled] = React.useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <header>
-      <a href="/">
-        <img />
-        <span>SEHATNO</span>
-      </a>
-      <ul>
-        {navLinks.map(({ name, link }) => (
-          <li key={name}>
-            <a href={link}>
-              <span>{name}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-      <Button text="Get Started" />
+    <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
+      <div className="inner">
+        <a href="#hero" className="group">
+          <img src="/public/logo.png" alt="logo" />
+          <span className="logo">SEHATNO</span>
+        </a>
+        <nav className="desktop">
+          <ul>
+            {navLinks.map(({ name, link }) => (
+              <li key={name} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Button text="Get Started" />
+      </div>
     </header>
   );
 };
